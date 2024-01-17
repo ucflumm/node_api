7c042340561e173d9da38487db4c82e274ae297f
ucflumm <ucflumm@gmail.com>
2024-01-17 17:37:02 +1100

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express =  require('express');
+const app = express();
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Hello Node API');
+})
+
+app.get('/blog', (req, res) => {
+    res.send('Hello Node API Blog');
+})
+
+app.listen(3000, () => {
+    console.log('Node API app is running on port 3000');
+})
